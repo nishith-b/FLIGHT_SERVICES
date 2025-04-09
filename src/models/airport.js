cf1d477454f4dcffcc356b716c3cfaddac96f497
Nishith <nishith.one@gmail.com>
2025-04-09 20:43:53 +0530

    Added new migration to add foriegn key constarint in one to many relation

--- src/models/airport.js
+++ src/models/airport.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: "cityId",
         onDelete: "CASCADE",
-        onUpdate: "CASCADE",
       });
     }
   }
